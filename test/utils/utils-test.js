8be17913dc775cdfc9cf4bc877169cd5e6fb347a
Netguru - Michał Sajnóg <michal.sajnog@netguru.co>
2016-11-07 16:20:26 +0100

    Update tests

--- test/utils/utils-test.js
+++ test/utils/utils-test.js
@@ -1,13 +1,35 @@
-var assert = require('chai').assert;
-var babelEslint = require('babel-eslint');
-var utils = require('./../../rules/utils/utils');
+const assert = require('chai').assert;
+const babelEslint = require('babel-eslint');
+const utils = require('./../../rules/utils/utils');
 
 function parse(code) {
   return babelEslint.parse(code).body[0].expression;
 }
 
+describe('findNodes', function() {
+  const node = parse(`test = [
+    {test: "a"}, b, "c", [d, e], "f", "g", h, {test: "i"}, function() {}, [], new Date()
+  ]`).right.elements;
+
+  it('should find nodes based on their name', function() {
+    const literals = utils.findNodes(node, 'Literal');
+    const identifiers = utils.findNodes(node, 'Identifier');
+    const objects = utils.findNodes(node, 'ObjectExpression');
+    const functions = utils.findNodes(node, 'FunctionExpression');
+    const news = utils.findNodes(node, 'NewExpression');
+    const arrays = utils.findNodes(node, 'ArrayExpression');
+
+    assert.equal(literals.length, 3);
+    assert.equal(identifiers.length, 2);
+    assert.equal(objects.length, 2);
+    assert.equal(functions.length, 1);
+    assert.equal(news.length, 1);
+    assert.equal(arrays.length, 2);
+  });
+});
+
 describe('isIdentifier', function() {
-  var node = parse('test');
+  const node = parse('test');
 
   it('should check if node is identifier', function() {
     assert.ok(utils.isIdentifier(node));
@@ -15,7 +37,7 @@ describe('isIdentifier', function() {
 });
 
 describe('isLiteral', function() {
-  var node = parse('"test"');
+  const node = parse('"test"');
 
   it('should check if node is identifier', function() {
     assert.ok(utils.isLiteral(node));
@@ -23,7 +45,7 @@ describe('isLiteral', function() {
 });
 
 describe('isMemberExpression', function() {
-  var node = parse('test.value');
+  const node = parse('test.value');
 
   it('should check if node is member expression', function() {
     assert.ok(utils.isMemberExpression(node));
@@ -31,7 +53,7 @@ describe('isMemberExpression', function() {
 });
 
 describe('isCallExpression', function() {
-  var node = parse('test()');
+  const node = parse('test()');
 
   it('should check if node is call expression', function() {
     assert.ok(utils.isCallExpression(node));
@@ -39,7 +61,7 @@ describe('isCallExpression', function() {
 });
 
 describe('isObjectExpression', function() {
-  var node = parse('test = {}').right;
+  const node = parse('test = {}').right;
 
   it('should check if node is identifier', function() {
     assert.ok(utils.isObjectExpression(node));
@@ -47,7 +69,7 @@ describe('isObjectExpression', function() {
 });
 
 describe('isArrayExpression', function() {
-  var node = parse('test = []').right;
+  const node = parse('test = []').right;
 
   it('should check if node is array expression', function() {
     assert.ok(utils.isArrayExpression(node));
@@ -55,7 +77,7 @@ describe('isArrayExpression', function() {
 });
 
 describe('isFunctionExpression', function() {
-  var node = parse('test = function () {}').right;
+  const node = parse('test = function () {}').right;
 
   it('should check if node is function expression', function() {
     assert.ok(utils.isFunctionExpression(node));
@@ -63,7 +85,7 @@ describe('isFunctionExpression', function() {
 });
 
 describe('isNewExpression', function() {
-  var node = parse('new Date()');
+  const node = parse('new Date()');
 
   it('should check if node is new expression', function() {
     assert.ok(utils.isNewExpression(node));
@@ -71,7 +93,7 @@ describe('isNewExpression', function() {
 });
 
 describe('isCallWithFunctionExpression', function() {
-  var node = parse('mysteriousFnc(function(){})');
+  const node = parse('mysteriousFnc(function(){})');
 
   it('should check if node is call with function expression', function() {
     assert.ok(utils.isCallWithFunctionExpression(node));
@@ -79,7 +101,7 @@ describe('isCallWithFunctionExpression', function() {
 });
 
 describe('isThisExpression', function() {
-  var node = parse('this');
+  const node = parse('this');
 
   it('should check if node is "this" expression', function() {
     assert.ok(utils.isThisExpression(node));
@@ -87,7 +109,7 @@ describe('isThisExpression', function() {
 });
 
 describe('getSize', function() {
-  var node = parse('some = {\nfew: "line",\nheight: "statement",\nthat: "should",\nhave: "6 lines",\n};');
+  const node = parse('some = {\nfew: "line",\nheight: "statement",\nthat: "should",\nhave: "6 lines",\n};');
 
   it('should check size of given expression', function() {
     assert.equal(utils.getSize(node), 6);
@@ -96,15 +118,15 @@ describe('getSize', function() {
 
 describe('parseCallee', function() {
   it('should parse calleeExpression', function() {
-    var node = parse('Ember.computed.or("asd", "qwe")');
-    var parsedCallee = utils.parseCallee(node);
+    const node = parse('Ember.computed.or("asd", "qwe")');
+    const parsedCallee = utils.parseCallee(node);
     assert.equal(parsedCallee.length, 3, 'it has 3 elements in array');
     assert.deepEqual(parsedCallee, ['Ember', 'computed', 'or']);
   });
 
   it('should parse newExpression', function() {
-    var node = parse('new Ember.A()');
-    var parsedCallee = utils.parseCallee(node);
+    const node = parse('new Ember.A()');
+    const parsedCallee = utils.parseCallee(node);
     assert.equal(parsedCallee.length, 2, 'it has 2 elements in array');
     assert.deepEqual(parsedCallee, ['Ember', 'A']);
   });
@@ -112,8 +134,8 @@ describe('parseCallee', function() {
 
 describe('parseArgs', function() {
   it('should parse arguments', function() {
-      var node = parse('Ember.computed("asd", "qwe", "zxc", function() {})');
-      var parsedArgs = utils.parseArgs(node);
+      const node = parse('Ember.computed("asd", "qwe", "zxc", function() {})');
+      const parsedArgs = utils.parseArgs(node);
       assert.equal(parsedArgs.length, 3);
       assert.deepEqual(parsedArgs, ['asd', 'qwe', 'zxc']);
   });
